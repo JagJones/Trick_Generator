555728010f7b5688f5f330587fb488d980899754
Jaguar_Jones <h.aubrey47@gmail.com>
2020-11-02 21:13:38 -0500

    Adding burg menu icon.

--- T_T.js
+++ T_T.js
@@ -1,6 +1,14 @@
 //----- To-Dos -----//
 
 //----- Hamburger Menu (<560px) -----//
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
 
 //----- Loading Gif -----//
 

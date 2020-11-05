8f75b233c49a0aa78ddfd62d315b3915c6cd56c2
Jaguar_Jones <h.aubrey47@gmail.com>
2020-11-04 22:54:51 -0500

    Made changes to all style aspects in CSS sheets.

--- T_T.js
+++ T_T.js
@@ -12,32 +12,6 @@ function myFunction() {
 
 //----- Loading Gif -----//
 
-//----- Login & Password ----- //
-
-// const password = "LetMeIn";
-// let message = "Nope, that's incorrect.";
-
-// for ( let i = 5; i >= 1; i-- ) {  
-//   let guess = prompt(`Please enter your password. You have ${i} tries.`);
-//   if ( guess === password ) {
-//     message = "That's correct, come on in";
-//     break;
-//   }
-// }
-
-// alert(message);
-// const password = "LetMeIn";
-// let message = "Nope, that's incorrect.";
-
-// for ( let i = 5; i >= 1; i-- ) {  
-//   let guess = prompt(`Please enter your password. You have ${i} tries.`);
-//   if ( guess === password ) {
-//     message = "That's correct, come on in";
-//     break;
-//   }
-// }
-
-// alert(message);
 
 
 

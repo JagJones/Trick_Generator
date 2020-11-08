c2b30ee36830902e089d03a9907480079b276ca6
Jaguar_Jones <h.aubrey47@gmail.com>
2020-11-08 17:01:15 -0500

    Added JS, and design elements.

--- T_T.js
+++ T_T.js
@@ -1,7 +1,7 @@
 //----- To-Dos -----//
 
-//----- Hamburger Menu (<560px) -----//
-function myFunction() {
+//--- Hamburger Menu (<560px) ---//
+function burgNav() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -10,7 +10,23 @@ function myFunction() {
     }
   }
 
-//----- Loading Gif -----//
+//--- Generators ---//
+//--- CLicking the buttons should open a random video from a predetermied list.--//
+$(document).ready(function() {
+
+    var videos = [
+    'KyY5yJw8Sbg',
+    'Te4wx4jtiEA',
+    'efTj6UYzvk4'
+    ];
+    
+    var index=Math.floor(Math.random() * videos.length);
+    var html='<div id="video"><h3>Random Video</h3><iframe width="720" height="480" src="http://www.youtube.com/embed/ ' + videos[index] + ' + "&autoplay=1" " frameborder="0" allowfullscreen></iframe></div>';
+    document.write(html);
+    
+    });//-->
+
+//--- Loading Gif ---//
 
 
 

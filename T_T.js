50384591bdc4cbc01cc9776b222a408191a8eef5
Jaguar_Jones <h.aubrey47@gmail.com>
2020-11-12 13:07:28 -0500

    Making sure everything's up to date.

--- T_T.js
+++ T_T.js
@@ -1,39 +1,64 @@
+
 //----- To-Dos -----//
 
 //--- Hamburger Menu (<560px) ---//
 function burgNav() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
 //--- Generators ---//
-//--- Clicking the buttons should open a random video from a predetermied list.--//
-$(document).ready(function() {
 
-function playRandoTrick() {
-    var trickVids = [
-    'KyY5yJw8Sbg',
-    'Te4wx4jtiEA',
-    'efTj6UYzvk4'
-    ];
-    
-    var index=Math.floor(Math.random() * trickVids);
-    var html='<div id="video"><h3>Random Video</h3><iframe width="720" height="480" src="http://www.youtube.com/embed/ ' + videos[index] + ' + "&autoplay=1" " frameborder="0" allowfullscreen></iframe></div>';
-    document.write(html);
-    
-    });
+//- Tricks -//
 
-    var mywin = window.open(...); 
-    mywin.somearray = [1,2,3];
+// function playRandomTVid(trickVids) {
+//  
+//   }
+//   return trickVids;
+// }
+
+function playRandomTVid(ytLink){
+//   var  = document.getElementById(#trick_button):
+//   var ytLink = window.open(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${trickVids}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+//   var currentIndex = trickVids.length, temporaryValue, randomIndex;
+//   while (0 !== currentIndex) {
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex -= 1;
+//       temporaryValue = trickVids[currentIndex];
+//       trickVids[currentIndex] = trickVids[randomIndex];
+//       trickVids[randomIndex] = temporaryValue;
+//   }
+// }
+
+var trickVids = [
+  `KyY5yJw8Sbg"`,
+  `6TZUEOV9-nQ"`,
+  `0vtn8NhofOw"`,
+  `NMRRLUyAIyw"`
+  ];
+
+  document.querySelector('').innerHTML
+
+// var trickVids=["a","b","c","d","e"];
+// randomArrayShuffle(trickVids); 
+
+// var pathArray = window.location.pathname.split('/');
+  
+  // 
+  // document.window.open(html);
+  
+ 
+
+  // var mywin = window.open(...); 
+  // mywin.somearray = [1,2,3];
+
+//- Games -//
+
+// function playRandoGame() {
+//   const gameVids = [
 
 //--- Loading Gif ---//
-
-
-
-
-
-

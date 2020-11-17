1da7c4918da924dc97ad3a8d0224eab8c4a098c6
Jaguar_Jones <h.aubrey47@gmail.com>
2020-11-16 20:06:26 -0500

    Tidying up, added a README, and removed some fluff.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Project_Name
+Trick_Generator
+
+## Description
+```
+A web pg to find new tricks or games to play with your dog. Pick whether you want to learn a trick or a game, then click the corresponding button to get a random video from either category.
+
+```
+
+
+
+## Custom CSS Classes
+```
+The class(es) I created are:
+
+1. class name ex( .main-info-section )
+.. what class does, ex( adds padding & changes background color of .main-info-section )
+
+2.
+
+```
+
+
+
+## Custom JavaScript Functions
+```
+The javascript functions I created are:
+
+1. function name ex( animate() )
+.. what function does ex( animate() is used to move the elements in the info div across the screen)
+
+```
--- T_T.js
+++ T_T.js
@@ -1,6 +1,4 @@
 
-//----- To-Dos -----//
-
 //--- Hamburger Menu (<560px) ---//
 function burgNav() {
   var x = document.getElementById("myLinks");
@@ -15,50 +13,34 @@ function burgNav() {
 
 //- Tricks -//
 
-// function playRandomTVid(trickVids) {
-//  
-//   }
-//   return trickVids;
-// }
-
-function playRandomTVid(ytLink){
-//   var  = document.getElementById(#trick_button):
-//   var ytLink = window.open(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${trickVids}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-//   var currentIndex = trickVids.length, temporaryValue, randomIndex;
-//   while (0 !== currentIndex) {
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-//       temporaryValue = trickVids[currentIndex];
-//       trickVids[currentIndex] = trickVids[randomIndex];
-//       trickVids[randomIndex] = temporaryValue;
-//   }
-// }
-
-var trickVids = [
-  `KyY5yJw8Sbg"`,
-  `6TZUEOV9-nQ"`,
-  `0vtn8NhofOw"`,
-  `NMRRLUyAIyw"`
-  ];
-
-  document.querySelector('').innerHTML
-
-// var trickVids=["a","b","c","d","e"];
-// randomArrayShuffle(trickVids); 
-
-// var pathArray = window.location.pathname.split('/');
-  
-  // 
-  // document.window.open(html);
-  
- 
-
-  // var mywin = window.open(...); 
-  // mywin.somearray = [1,2,3];
+function playRandomTVid() {
+  let trickVids = [
+    `KyY5yJw8Sbg`,
+    `6TZUEOV9-nQ`,
+    `0vtn8NhofOw`,
+    `NMRRLUyAIyw`
+    ];
+  const randomInt = Math.floor(Math.random() * trickVids.length)
+  console.log(trickVids[randomInt])
+  window.open(`https://www.youtube.com/embed/${trickVids[randomInt]}`)
+}
 
 //- Games -//
 
-// function playRandoGame() {
-//   const gameVids = [
+function playRandomGVid() {
+  let gameVids = [
+    `ZtpLvumSTzI`,
+    `stdnfuFbmUc`,
+    `2v7CcqAHm-4`,
+    `A6bEHd2q2Po`
+    ];
+  const randomInt = Math.floor(Math.random() * gameVids.length)
+  console.log(gameVids[randomInt])
+  window.open(`https://www.youtube.com/embed/${gameVids[randomInt]}`)
+}
 
-//--- Loading Gif ---//
+//--- Buttons ---//
+
+
+
+
